94ec8859369d38ba6301b1f2d92a269117772858
Cesaria Jose <cesariajose@Cesarias-MacBook-Air.local>
2025-09-19 14:52:18 -0700

    changes to text phrases

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function App() {
         <>
             <main className='header_section'>
                 <h1 className='title'>Tech Girl Reads</h1>
-                <p className='slogan'>catch phrase</p>
+                <p className='slogan'>for the girls who love technology</p>
             </main>
             <div className='books_section'>
                 <div className='books_section_excerpts'>
                     <h2 className='book_section_title'>Starter Girl Pack</h2>
                     <p className='book_section_description'>
-                        some cool description of books for new girls to
-                        technology
+                        This is a starter pack for new girls in technology.
+                        Check them out with the learn more button
                     </p>
                 </div>
 
